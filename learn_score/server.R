0e3122ce3be2b7689fb2547c27a8acc2d4b69044
HANDEL Ian <ian.handel@ed.ac.uk>
2018-12-07 13:16:55 +0000

    dev

--- learn_score/server.R
+++ learn_score/server.R
@@ -1,26 +1,33 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Parse Learn VPH scores - shiny server
 
 library(shiny)
+library(readxl)
+library(tidyverse)
+library(DT)
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+shinyServer(function(input, output, session) {
+  dat_raw <- reactive({
+    infile <- input$file
+
+    if (is.null(infile)) {
+      return(NULL)
+    }
+
+    readxl::read_excel(path = infile$datapath, sheet = 1)
   })
-  
+
+  dat_proc <- reactive({
+    dat_raw()
+  })
+
+
+  output$data_raw <- DT::renderDataTable(
+    dat_raw()
+  )
+
+  output$data_proc <- DT::renderDataTable(
+    dat_proc()
+  )
 })
--- learn_score/ui.R
+++ learn_score/ui.R
@@ -1,33 +1,34 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Parse Learn VPH scores - shiny ui
 
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  titlePanel("Process Learn VPH Scores"),
+
+  tabsetPanel(
+    tabPanel(
+      "Import",
+      fileInput(
+        inputId = "file",
+        label = "Choose input file"
+      )
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+
+    tabPanel(
+      "Raw data",
+      mainPanel(
+        DT::dataTableOutput(outputId = "data_raw")
+      )
+    ),
+
+    tabPanel(
+      "Processed data",
+      mainPanel(
+        DT::dataTableOutput(outputId = "data_proc")
+      )
     )
   )
 ))
